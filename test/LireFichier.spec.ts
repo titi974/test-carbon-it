c69a81daa15ed1206e82c822e13292cf37bf0e1a
titi974 <reunionnais1@gmail.com>
2021-03-19 10:53:09 +0100

    Lire le fichier test

---
+++ test/LireFichier.spec.ts
@@ -0,0 +1,33 @@
+import path from "path";
+
+describe('lireFichier', () => {
+    describe('trouver le fichier', () => {
+        const root = path.join(path.dirname(__dirname), 'test', 'files')
+        it('error', () => {
+            // Given
+            const file = 'badFile.txt'
+            // When
+            try {
+                lireFichier(path.join(root, file))
+                expect(true).toEqual(false)
+            } catch (e) {
+                // Then
+                expect(e.message).toMatch('no such file or directory')
+            }
+        })
+        it('success', () => {
+            // Given
+            const file = 'chasseAuxTresors.txt'
+            // When
+            const data = lireFichier(path.join(root, file))
+            // Then
+            expect(data).toEqual("C - 3 - 4\n" +
+                "M - 1 - 0\n" +
+                "M - 2 - 1\n" +
+                "T - 0 - 3 - 2\n" +
+                "T - 1 - 3 - 3\n" +
+                "A - Lara - 1 - 1 - S - AADADAGGA" +
+                "\n")
+        })
+    })
+});
